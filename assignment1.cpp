1c89da86622257c31dace24bdd681cc400b40b37
kuimetra <alinaaartemiuk@gmail.com>
2023-03-08 18:56:03 +0100

    example with outputs

--- assignment1.cpp
+++ assignment1.cpp
@@ -84,51 +84,24 @@ void mergeSort(ull* arr, int start, int end)
     }
 }
 
-//int binarySearch(int arr[], int l, int r, int x)
-//{
-//    if (r >= l) 
-//    {
-//        int mid = l + (r - l) / 2;
-//        if (arr[mid] == x)
-//            return mid;
-//
-//        if (arr[mid] > x)
-//            return binarySearch(arr, l, mid - 1, x);
-//
-//        return binarySearch(arr, mid + 1, r, x);
-//    }
-//    return -1;
-//}
+ull sum(ull* arr, int size)
+{
+    ull sum = 0;
+    for (int i = 0; i < size; i++)
+    {
+        sum += arr[i];
+    }
+    return sum;
+}
 
-//void bucketSort(float* array, int size) {
-//    vector<float> bucket[size];
-//    for (int i = 0; i < size; i++) 
-//    {
-//        bucket[int(size * array[i])].push_back(array[i]);
-//    }
-//    for (int i = 0; i < size; i++)
-//    {
-//        sort(bucket[i].begin(), bucket[i].end());
-//    }
-//    int index = 0;
-//    for (int i = 0; i < size; i++)
-//    {
-//        while (!bucket[i].empty()) {
-//            array[index++] = *(bucket[i].begin());
-//            bucket[i].erase(bucket[i].begin());
-//        }
-//    }
-//}
-
-void sampleSort(ull* arr, int n, int p) {
-    // Divide the array into p partitions
-    int partitionSize = n / p;
+ull** getPartitions(ull* arr, int size, int p)
+{
     ull** partitions = new ull * [p];
 
     for (int i = 0; i < p; i++)
     {
-        int start = i * partitionSize;
-        int end = start + partitionSize - 1;
+        int start = i * size;
+        int end = start + size - 1;
 
         // allocate memory for partition
         partitions[i] = new ull[end - start + 1];
@@ -140,66 +113,43 @@ void sampleSort(ull* arr, int n, int p) {
         }
     }
 
-    cout << "Sorted partitions:" << endl;
-    for (int i = 0; i < p; i++)
-    {
-        mergeSort(partitions[i], 0, partitionSize - 1);
+    return partitions;
+}
 
-        for (int j = 0; j < partitionSize; j++)
-        {
-            cout << partitions[i][j] << " ";
-        }
-        cout << endl;
-    }
-
-    int div = p - 1;
-    int nonDiv = partitionSize - div;
-    int dividersSize = p * div;
+ull* getDividers(ull** partitions, int div, int dividersSize, int size, int p)
+{
+    int nonDiv = size - div;
     ull* dividers = new ull[dividersSize];
 
-    // cout << "\ndiv / non-div : " << div << " / " << nonDiv << endl;
-
-    double step = (double)nonDiv / (double)(div + 1);
-    int startIndex = (step < 1) ? 0 : (int)round(step);
-    step = (step < 1) ? 1 : (int)round(step);
+    int step = nonDiv / p + 1;
 
     int divIndex = 0;
 
-    // cout << "start index, step : " << startIndex << ", " << step << endl << endl;
-
     for (int i = 0; i < p; i++) // for each bucket
     {
-        int bucketDivCounter = 0;
+        int r = nonDiv % p;
+        int q = -1;
 
-        for (int j = startIndex; j < partitionSize; j += step + 1) // for each div
+        for (int j = 1; j < p; j++) // for each div
         {
-            if (bucketDivCounter == div) break;
-            dividers[divIndex++] = partitions[i][j];
-            bucketDivCounter++;
-        }
-
-        if (bucketDivCounter != div)
-        {
-            int j = startIndex + 1;
-            while (bucketDivCounter != div)
+            q += step;
+            dividers[divIndex++] = partitions[i][q];
+            if (r > 0)
             {
-                dividers[divIndex++] = partitions[i][j];
-
-                j += step + 1;
-                bucketDivCounter++;
+                q++;
+                r--;
             }
         }
     }
 
-    cout << "\nSorted dividers: ";
-    mergeSort(dividers, 0, dividersSize - 1);
-    printArray(dividers, dividersSize);
-    cout << endl;
+    return dividers;
+}
 
-    int bucketDelSize = p - 1;
-    ull* bucketDel = new ull[bucketDelSize];
+ull* getBucketDel(ull* dividers, int div, int dividersSize, int p)
+{
+    ull* bucketDel = new ull[div];
 
-    int notDel = dividersSize - bucketDelSize;
+    int notDel = dividersSize - div;
     int delStep = notDel / p + 1;
     int rem = notDel % p;
     int k = -1;
@@ -217,15 +167,193 @@ void sampleSort(ull* arr, int n, int p) {
         }
     }
 
-    cout << "Pick every p'th divider: ";
-    printArray(bucketDel, bucketDelSize);
+    return bucketDel;
+}
+
+ull** getSizeMatrix(ull** partitions, ull* bucketDel, int partitionSize, int div, int p)
+{
+    ull** sizeMat = new ull * [p];
+
+    for (int i = 0; i < p; i++)
+    {
+        sizeMat[i] = new ull[p];
+        for (int k = 0; k < div; k++)
+        {
+            sizeMat[i][k] = 0;
+            for (int j = 0; j < partitionSize; j++)
+            {
+                if (partitions[i][j] < bucketDel[k])
+                {
+                    sizeMat[i][k] += 1;
+                }
+            }
+        }
+    }
+
+    for (int i = 0; i < p; i++)
+    {
+        for (int j = 1; j < div; j++)
+        {
+            sizeMat[i][j] -= sum(sizeMat[i], j);
+        }
+        sizeMat[i][div] = partitionSize - sum(sizeMat[i], div);
+    }
+
+    return sizeMat;
+}
+
+ull* sampleSort(ull* arr, int n, int p) {
+    // Divide the array into p partitions
+    int partitionSize = n / p;
+    int div = p - 1;
+    int dividersSize = p * div;
+
+    ull** partitions = getPartitions(arr, partitionSize, p);
+    for (int i = 0; i < p; i++)
+    {
+        mergeSort(partitions[i], 0, partitionSize - 1);
+        printArray(partitions[i], partitionSize);
+    }
+    cout << endl;
+
+    ull* dividers = getDividers(partitions, div, dividersSize, partitionSize, p);
+    mergeSort(dividers, 0, dividersSize - 1);
+    for (int i = 0; i < dividersSize; i++)
+    {
+        cout << dividers[i] << " ";
+    }
+    cout << endl << endl;
+
+    ull* bucketDel = getBucketDel(dividers, div, dividersSize, p);
+    for (int i = 0; i < div; i++)
+    {
+        cout << bucketDel[i] << " ";
+    }
+    cout << endl << endl;
+
+    ull** sizeMat = getSizeMatrix(partitions, bucketDel, partitionSize, div, p);
+    for (int i = 0; i < p; i++)
+    {
+        for (int j = 0; j < p; j++)
+        {
+            cout << sizeMat[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+
+    ull* bucketSize = new ull[p];
+    for (int i = 0; i < p; i++)
+    {
+        int colSum = 0;
+        for (int j = 0; j < p; j++)
+        {
+            colSum += sizeMat[j][i];
+        }
+        bucketSize[i] = colSum;
+    }
+    for (int i = 0; i < p; i++)
+    {
+        cout << bucketSize[i] << " ";
+    }
+    cout << endl << endl;
+
+    ull** bucket = new ull * [p];
+    for (int i = 0; i < p; i++)
+    {
+        bucket[i] = new ull[bucketSize[i]];
+    }
+
+    bool** flags = new bool* [p];
+    for (int i = 0; i < p; i++)
+    {
+        flags[i] = new bool[partitionSize];
+        for (int j = 0; j < partitionSize; j++)
+        {
+            flags[i][j] = false;
+        }
+    }
+
+    ull* bucketIndices = new ull[p];
+    for (int i = 0; i < p; i++)
+    {
+        bucketIndices[i] = 0;
+    }
+
+    for (int i = 0; i < p; i++)
+    {
+        for (int j = 0; j < div; j++)
+        {
+            for (int k = 0; k < partitionSize; k++)
+            {
+                if (partitions[i][k] < bucketDel[j] && !flags[i][k])
+                {
+                    int bucketElemIndex = bucketIndices[j];
+                    bucket[j][bucketElemIndex] = partitions[i][k];
+                    flags[i][k] = true;
+                    bucketIndices[j] += 1;
+                }
+            }
+        }
+    }
+
+    for (int i = 0; i < p; i++)
+    {
+        for (int j = 0; j < partitionSize; j++)
+        {
+            if (partitions[i][j] >= bucketDel[div - 1])
+            {
+                int bucketElemIndex = bucketIndices[p - 1];
+                bucket[div][bucketElemIndex] = partitions[i][j];
+                bucketIndices[div] += 1;
+            }
+        }
+    }
+
+    for (int i = 0; i < p; i++)
+    {
+        cout << "Bucket [" << i + 1 << "] : ";
+        for (int j = 0; j < bucketSize[i]; j++)
+        {
+            cout << bucket[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+
+    for (int i = 0; i < p; i++)
+    {
+        mergeSort(bucket[i], 0, bucketSize[i]);
+    }
+
+    ull* sortedArray = new ull[n];
+    int ind = 0;
+    for (int i = 0; i < p; i++)
+    {
+        for (int j = 0; j < bucketSize[i]; j++)
+        {
+            sortedArray[ind++] = bucket[i][j];
+        }
+    }
+
     for (int i = 0; i < p; i++)
     {
         delete[] partitions[i];
+        delete[] sizeMat[i];
+        delete[] bucket[i];
+        delete[] flags[i];
     }
 
     delete[] partitions;
     delete[] dividers;
+    delete[] bucketDel;
+    delete[] sizeMat;
+    delete[] bucketSize;
+    delete[] bucket;
+    delete[] flags;
+    delete[] bucketIndices;
+
+    return sortedArray;
 }
 
 int main()
@@ -252,15 +380,13 @@ int main()
     printArray(arr, n);
     cout << endl;
 
-    // mergeSort(arr, 0, n - 1);
-
     double startTime = omp_get_wtime();
-    sampleSort(arr, n, p);
-    cout << endl;
+    arr = sampleSort(arr, n, p);
     double endTime = omp_get_wtime();
 
-    //printArray(arr, n);
-    //cout << endl;
+    cout << "Sorted array: ";
+    printArray(arr, n);
+    cout << endl;
 
     // Verify that the array is sorted
     for (int i = 0; i < n - 1; i++)
