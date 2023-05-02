bf41a587c8dfea73a226a76ffc0f40dd33499602
kuimetra <alinaaartemiuk@gmail.com>
2023-05-02 13:27:34 +0200

    printing and writing func

--- stable marriage problem/matrix_generator.h
+++ stable marriage problem/matrix_generator.h
@@ -1,5 +1,31 @@
 using namespace std;
 
+void print_matrix(int n, int **mat)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << mat[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+void write_matrix_to_file(ofstream &f, int n, int **mat)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            f << mat[i][j] << " ";
+        }
+        f << endl;
+    }
+    f << endl;
+}
+
 void generate_pref_matrix(int n, int **man_mat, int **wom_mat)
 {
 #pragma omp parallel for
